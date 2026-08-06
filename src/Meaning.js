87a57038950d7a6613a9013bf63c05506b72e781
Lebohang2 <llebo829@example.com>
2026-08-06 15:53:53 +0200

    Add Synonyms component and integrate it into Meaning component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props){
     return (
-        <div className="Meaning">
-            <h3>{props.meaning.partOfSpeech}</h3>
-            <p>{props.meaning.definition}</p>
-            {props.meaning.example && (
-            <p>
-            <em>Example: {props.meaning.example}</em>
-            </p>
-            )}
-        </div>
+       <div className="Meaning">
+  <h3>{props.meaning.partOfSpeech}</h3>
+
+  <p>{props.meaning.definition}</p>
+
+  {props.meaning.example && (
+    <p>
+      <em>Example: {props.meaning.example}</em>
+    </p>
+  )}
+
+  <Synonyms synonyms={props.meaning.synonyms} />
+</div>
     );
 }
